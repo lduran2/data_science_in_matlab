8bb23d1bb129f896717ee520848a3a88ecb39ebc
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2023-02-04 09:54:35 -0500

    displaying column names

--- lab03_linearRegression.m
+++ lab03_linearRegression.m
@@ -1,14 +1,24 @@
 %% Lab 3: Linear Regression
 %
-% This example shows how to use linear regression to predict house prices.
-% It teaches how to preprocess data, then split the data set into the
-% training set and testing set, use the training set to train a model
-% and evaluate that model based on the testing set.
+% This example shows how to use linear regression to predict house
+% prices.  More specifically, it teaches how to preprocess data, then
+% split the dataset into the training set and testing set.  Afterwards,
+% is teaches how to use the training set to train a model and evaluate
+% that model based on the testing set.
 %
-% After studying the prediction of housing prices, practing using linear
-% regression to predict medical cost from the data in "insurance.csv".
+% After studying the prediction of housing prices, practing using
+% linear regression to predict medical cost from the data in
+% "./insurance.csv".
 
 %% 1. Example: Linear Regression for House Price Prediction
+% We will use the house price dataset "./housing.csv" to predict
+% housing prices.  This dataset includes the attributes
+
+% clear the workspace of variables
+clear;
+% load the housing data
+T = readtable('housing.csv');
+% display the column names
+disp(T.Properties.VariableNames')
+
 %% 1.1 Preprocess the raw data
-
-
