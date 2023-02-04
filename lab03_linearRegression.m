eecd5262c6315f815cbedad6b5bf6aee02ae4ffb
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2023-02-04 09:08:41 -0500

    description of the example and exercise

---
+++ lab03_linearRegression.m
@@ -0,0 +1,14 @@
+%% Lab 3: Linear Regression
+%
+% This example shows how to use linear regression to predict house prices.
+% It teaches how to preprocess data, then split the data set into the
+% training set and testing set, use the training set to train a model
+% and evaluate that model based on the testing set.
+%
+% After studying the prediction of housing prices, practing using linear
+% regression to predict medical cost from the data in "insurance.csv".
+
+%% 1. Example: Linear Regression for House Price Prediction
+%% 1.1 Preprocess the raw data
+
+
